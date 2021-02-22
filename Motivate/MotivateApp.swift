7cda3b555512bd378ed89cca75c913d8022606da
Mantas <mantas.skeiverys@tesonet.com>
2021-02-22 19:13:21 +0200

    Initial Commit

---
+++ Motivate/MotivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivateApp.swift
+//  Motivate
+//
+//  Created by T1001 on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct MotivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
