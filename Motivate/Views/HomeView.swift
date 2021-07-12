03ddbce7a693ede6161873a6e9d4beadefd9f853
Mantas <mantas.skeiverys@tesonet.com>
2021-07-13 01:11:25 +0300

    Home item

--- Motivate/Views/HomeView.swift
+++ Motivate/Views/HomeView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 struct HomeView: View {
         
     var body: some View {
-        Text("Hello from home!")
+        ScrollView {
+            HomeItem(title: "Create an inspiration", imageName: "nature")
+        }
+        .setSmallPadding(.vertical)
+    }
+}
+
+private struct HomeItem: View {
+    
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        Button {
+            print("Clicked")
+        } label: {
+            ZStack {
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(8)
+                Text(title)
+                    .font(.dancingScript, size: 26)
+                    .foregroundColor(.white)
+                    .setBigPadding(.all)
+                    .align(.bottom, .trailing)
+            }
+            .setSmallPadding(.horizontal)
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
